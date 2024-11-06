de8ee312dc417f1e0b0e059265a3ee19263b12f7
serjeph <engr.jeffmanzanillo@gmail.com>
2024-11-06 21:16:26 +0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeCollect
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public string SpreadsheetId { get; set; }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TimeCollect
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Employee> Employees { get; set; }
+
+
+        // add other properties and commands here later...
+
+
+        public MainViewModel()
+        {
+            Employees = new ObservableCollection<Employee>();
+        }
+
+        // Implement INotifyPropertyChange interface later...
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
